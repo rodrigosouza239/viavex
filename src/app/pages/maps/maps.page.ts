30a397afa1525c502b544559b0082761e36f8277
rodrigo soares <walter_rodrigo238@hotmail.com>
2021-02-24 16:05:25 -0400

    viavexmobile

--- src/app/pages/maps/maps.page.ts
+++ src/app/pages/maps/maps.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { ViewChild,ElementRef } from '@angular/core'
 
+declare var google:any;
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.page.html',
   styleUrls: ['./maps.page.scss'],
 })
-export class MapsPage implements OnInit {
+export class MapsPage {
+
+  map:any;
+  @ViewChild('map', {read:ElementRef, static:false}) mapRef:ElementRef;
 
   constructor() { }
 
-  ngOnInit() {
+  ionViewDidEnter(){
+    this.showMap();
   }
+
+   showMap(){
+     const location = new google.maps.LatLng(-3.0925805,-59.9446353);
+     const options = {
+       center: location,
+       zoom:15,
+       disableDefaultUI:true
+     }
+     this.map = new google.maps.Map(this.mapRef.nativeElement, options)
+   }
 
 }
